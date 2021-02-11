1038f8284afc6be4e2333fafdb8b178675e43016
Pablo Arrieta <pablou2015@hotmail.com>
2021-02-11 15:30:20 -0500

    modal product detail

--- src/app/components/dummies/product-card/product-card.component.ts
+++ src/app/components/dummies/product-card/product-card.component.ts
@@ -1,17 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductDetailComponent } from '@Dummies/product-detail/product-detail.component';
 import { Product } from '@Utils/types/product.type';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent {
   @Input() product!: Product;
+  public modalRef!: BsModalRef;
 
-  constructor() {}
+  constructor(private modalService: BsModalService) {}
 
-  ngOnInit(): void {}
-
-  openDetail() {}
+  openDetail() {
+    const initialState = {
+      product: this.product,
+    };
+    this.modalService.show(ProductDetailComponent, {
+      class: 'modal-dialog-centered modal-lg',
+      initialState,
+    });
+  }
 }
--- src/app/components/dummies/product-detail/product-detail.component.ts
+++ src/app/components/dummies/product-detail/product-detail.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Product, ProductDetail } from '@Utils/types/product.type';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styles: [
-  ]
+  styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
+  public product!: Product;
+  public detail!: ProductDetail;
+  constructor(public modalRef: BsModalRef) {}
 
   ngOnInit(): void {
+    this.detail = {
+      product: this.product,
+      quantity: 1,
+    };
   }
 
+  changeQuantity(control: string): void {
+    if (control === 'add') this.detail.quantity++;
+    else {
+      this.detail.quantity === 1 ? 1 : this.detail.quantity--;
+    }
+  }
 }
